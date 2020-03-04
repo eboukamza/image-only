39c8b3e68145b50f3b67979964e363f26bc181fd
Elias Zain <ezain2t@gmail.com>
2020-03-04 14:08:12 +0100

    feature: make more responsive. replace ionic by bulma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { IonicModule } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { ImageService } from './image.service';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -15,7 +13,6 @@ import { environment } from '../environments/environment';
   entryComponents: [],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [ ImageService ],
