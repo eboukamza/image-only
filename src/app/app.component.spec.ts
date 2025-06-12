b50d009b8465097f9fc70281f3b1427bb49108b6
Elias Zain <ezain2t@gmail.com>
2025-06-12 18:05:25 +0200

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import { ImageService } from './image.service';
 import { Image } from './image.model';
 
 const createImage = () => new Image('test-url', 'test-type', 'test-name');
 
 describe('AppComponent tests', () => {
 
-  let component: AppComponent;
+  let app: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
-      providers: [ ImageService ]
-    });
-
     fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
+    app = fixture.componentInstance;
+  });
+
+  it('should create the app', () => {
+    expect(app).toBeTruthy();
   });
 
   it('should show images', () => {
-    component.images = [ createImage(), createImage(), createImage() ];
+    app.images = [ createImage(), createImage(), createImage() ];
     fixture.detectChanges();
 
     expect(fixture.nativeElement.querySelectorAll('.card')).toHaveLength(3);
