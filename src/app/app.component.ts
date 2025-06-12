04aa0b2a1dae0f910fe52823a7acd4d43434bf9f
Elias Zain <ezain2t@gmail.com>
2025-06-12 15:46:32 +0200

    update to angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { ImageService } from './image.service';
 import { generateRandomName } from './utils';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: [ 'app.component.sass' ]
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.sass'],
+    standalone: false
 })
 export class AppComponent {
 
